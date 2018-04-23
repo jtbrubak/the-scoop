c844cf05cb2ec92ef95bf535491e654cf026f8c2
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-23 17:45:33 -0400

    Add upvote and downvote routes

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ const routes = {
     'PUT': updateComment,
     'DELETE': deleteComment
   },
-  // '/comments/:id/upvote': {
-  //   'PUT': upvoteComment
-  // },
-  // '/comments/:id/downvote': {
-  //   'PUT': downvoteComment
-  // }
+  '/comments/:id/upvote': {
+    'PUT': upvoteComment
+  },
+  '/comments/:id/downvote': {
+    'PUT': downvoteComment
+  }
 };
 
 function getUser(url, request) {
@@ -318,6 +318,42 @@ function deleteComment(url, request) {
     response.status = 204;
   } else {
     response.status = 404;
+  }
+
+  return response;
+}
+
+function upvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment && database.users[username]) {
+    savedComment = upvote(savedComment, username);
+
+    response.body = {comment: savedComment};
+    response.status = 200;
+  } else {
+    response.status = 400;
+  }
+
+  return response;
+}
+
+function downvoteComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const username = request.body && request.body.username;
+  let savedComment = database.comments[id];
+  const response = {};
+
+  if (savedComment && database.users[username]) {
+    savedComment = downvote(savedComment, username);
+
+    response.body = {comment: savedComment};
+    response.status = 200;
+  } else {
+    response.status = 400;
   }
 
   return response;
