1b5327d32673203318a9bd48f22434deedec066c
Jeff Brubaker <jtbrubak1@gmail.com>
2018-04-23 17:34:22 -0400

    Add comments/:id PUT route

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ const routes = {
   },
   '/comments': {
     'POST': createComment
-  }
-  // '/comments/:id': {
-  //   'PUT': updateComment,
-  //   'DELETE': deleteComment
-  // },
+  },
+  '/comments/:id': {
+    'PUT': updateComment
+    // 'DELETE': deleteComment
+  },
   // '/comments/:id/upvote': {
   //   'PUT': upvoteComment
   // },
@@ -279,6 +279,26 @@ function createComment(url, request) {
     response.status = 201;
   } else {
     response.status = 400;
+  }
+
+  return response;
+}
+
+function updateComment(url, request) {
+  const id = Number(url.split('/').filter(segment => segment)[1]);
+  const savedComment = database.comments[id];
+  const requestComment = request.body && request.body.comment;
+  const response = {};
+
+  if (!id || !requestComment) {
+    response.status = 400;
+  } else if (!savedComment) {
+    response.status = 404;
+  } else {
+    savedComment.body = requestComment.body || savedComment.body;
+
+    response.body = {comment: savedComment};
+    response.status = 200;
   }
 
   return response;
